230319b002bd3a61ec9716500453de74a332aee1
andrea0reyna <145026191+andrea0reyna@users.noreply.github.com>
2023-09-21 08:02:13 -0600

    Create cod_olimpiadas1.py
    1

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,11 @@
+def suma (a,b):
+  x= a+b
+  return x 
+
+def resta (a,b):
+  x = a-b 
+print ("Dame el primer numero ")
+a= int (input())
+print("Dame el segundo numero ")
+b= int (input() )
+print ("La suma da, ",suma (a,b) , " y la resta da " ,resta (a,b))
